710966a63e96dbd7a81b40c05c3256cc74ca941f
Richard Roba <disturbed.iso@gmail.com>
2023-03-17 09:28:38 +0100

    [master]: package adaptions

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import { Slider } from "./dist/Slider.js";
 const sliders = document.querySelectorAll(".ls-slider");
 if (sliders.length) {
   sliders.forEach(sliderElement => {
-    new Slider(sliderElement, { infinite: false });
+    new Slider(sliderElement, { infinite: true });
   });
 }
